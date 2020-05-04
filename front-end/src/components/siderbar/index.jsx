0f1bdba839ceeb1674a3c95ac27584ba0294283a
ModifiedClass <185940341@qq.com>
2020-05-05 04:51:15 +0800

    hisx

--- front-end/src/components/siderbar/index.jsx
+++ front-end/src/components/siderbar/index.jsx
@@ -6,18 +6,38 @@ import { Menu, Icon} from 'antd'
 import './index.less'
 
 import menuList from '../../config/menuConfig'
+import {rGroups} from '../../api'
 //import memUtils from '../../utils/memUtils'
 import {setBreadCrum} from '../../redux/actions'
 
 const { SubMenu } = Menu
 
 class SiderBar extends Component{
-    
+    state={
+        menus:[]
+    }
+    initMenus=async()=>{
+        const groupids=this.props.user.group
+        const arr=[]
+        for(let i=0;i<groupids.length;i++){
+            const g={'_id':groupids[i]}
+            const result=await rGroups(g)
+            for(let j=0;j<result.data.length;j++){
+                let temp=result.data[j].menu.split(',')
+                for(let k=0;k<temp.length;k++){
+                    arr.push(temp[k])
+                }
+            }   
+        }
+        const menus=Array.from(new Set(arr))
+        this.setState({menus})
+    }
     hasAuth=(item)=>{
         const {key,isPublic}=item
-        const menus=this.props.user.group.menu
+        const menus=this.state.menus
+        //const menus=this.props.user.group.menu
         const username=this.props.user.username
-        if(username==='root' ||isPublic||menus.indexOf(key)!==-1){
+        if(username==='admin' ||isPublic||menus.indexOf(key)!==-1){
             return true
         }else if(item.children){
             return !!item.children.find(child=>menus.indexOf(child.key!==-1))
@@ -29,41 +49,44 @@ class SiderBar extends Component{
         const path=this.props.location.pathname
         return menuList.map(item=>{
             //if(this.hasAuth(item)){
-            if(!item.children){
-                //判断item是否当前对于得item，更新redux中BreadCrum状态
-                if(item.key===path||path.indexOf(item.key)===0){
-                    this.props.setBreadCrum(item.title)
+                if(!item.children){
+                    //判断item是否当前对于得item，更新redux中BreadCrum状态
+                    if(item.key===path||path.indexOf(item.key)===0){
+                        this.props.setBreadCrum(item.title)
+                    }
+                    return (
+                        <Menu.Item key={item.key} >
+                            <Link to={item.key} onClick={()=>this.props.setBreadCrum(item.title)}>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </Link>
+                        </Menu.Item>
+                    )
+                }else{
+                    //const cItem=item.children.find(cItem=>cItem.key===path)
+                    //产品路径有子类,将路径改为父类,菜单自动展开
+                    /*const cItem=item.children.find(cItem=>path.indexOf(cItem.key===0))
+                    if(cItem){
+                        this.openKey=item.key
+                    }*/
+                    return(
+                        <SubMenu key={item.key} title={
+                            <span>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </span>
+                        }>
+                        {this.getMenuNodes(item.children)}
+                        </SubMenu>
+                    )
                 }
-                return (
-                    <Menu.Item key={item.key} >
-                        <Link to={item.key} onClick={()=>this.props.setBreadCrum(item.title)}>
-                            <Icon type={item.icon} />
-                            <span>{item.title}</span>
-                        </Link>
-                    </Menu.Item>
-                )
-            }else{
-                //const cItem=item.children.find(cItem=>cItem.key===path)
-                //产品路径有子类,将路径改为父类,菜单自动展开
-                /*const cItem=item.children.find(cItem=>path.indexOf(cItem.key===0))
-                if(cItem){
-                    this.openKey=item.key
-                }*/
-                return(
-                    <SubMenu key={item.key} title={
-                        <span>
-                            <Icon type={item.icon} />
-                            <span>{item.title}</span>
-                        </span>
-                    }>
-                    {this.getMenuNodes(item.children)}
-                    </SubMenu>
-                )
-            }
             //}
         })
     }
     componentWillMount(){
+        this.initMenus()
+    }
+    componentDidMount(){
         this.menuNodes=this.getMenuNodes(menuList)
     }
     render(){
