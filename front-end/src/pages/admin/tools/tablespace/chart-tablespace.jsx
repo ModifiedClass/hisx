fb8c4bd3ade59d1c768f16aa6fdf2ac4fbb24651
ModifiedClass <185940341@qq.com>
2020-05-27 23:07:40 +0800

    hisx

--- front-end/src/pages/admin/tools/tablespace/chart-tablespace.jsx
+++ front-end/src/pages/admin/tools/tablespace/chart-tablespace.jsx
@@ -1,19 +1,27 @@
 import React,{Component} from 'react';
-import {Card,Progress } from 'antd'
+import {Card,Progress,Modal,Table } from 'antd'
 
 import {BASE_RED,BASE_BLUE,BASE_DYELLOW} from '../../../../utils/colors'
 
 
 export default class ChartTableSpace extends Component{
-  
+    state={
+        localTsName:'',
+        localTsNames:[],
+        isShow:false
+    }
+    getTsName=(content)=>{
+        console.log(content.target.innerHTML)
+    }
     render(){
         const{data,title}=this.props
+        const{localTsName}=this.state
         return(
             <Card title={title}>
                 {
                     data.map(d=>
                         <div key={d.name}>
-                            <span style={{width:'120px'}}>{d.name}</span>
+                            <span onClick={this.getTsName} style={{width:'120px'}}>{d.name}</span>
                             <Progress 
                             style={{width:'70%',float:'right'}} 
                             percent={d.value}
@@ -22,6 +30,21 @@ export default class ChartTableSpace extends Component{
                         </div>
                     )
                 }
+                <Modal
+                    title={localTsName}
+                    visible={isShow}
+                    onCancel={()=>{
+                        this.setState({isShow:false})
+                    }}
+                >
+                    <Table
+                        bordered
+                        rowKey='_id'
+                        loading={loading}
+                        dataSource={groups}
+                        columns={this.columns}
+                    />
+                </Modal>
             </Card>
         )
     }
